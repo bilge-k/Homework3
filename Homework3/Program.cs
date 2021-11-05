cf2391fd658e23ecba70698e0f604f79c14a377f
90553 <90553@LAPTOP-N21E6UKL>
2021-11-05 17:09:30 +0300

    Add project files.

---
+++ Homework3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Askerlik("erkek", 22);
+            BeslikSistem(88);
+        }
+        static void Askerlik(string cinsiyet, int yas)
+        {
+            string a = "asker kaçağı>:/ ";
+            string b = "vatananına milletine hayırlı*";
+            if (cinsiyet == "erkek" && yas > 20)
+            {
+                Console.WriteLine(a);
+
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+        static void BeslikSistem(int p, double k=0.05)
+        {
+            double s;
+            string d = "Puanınız: ";
+           
+            s = k * p;
+            Console.WriteLine(d+s);
+
+        }
+    }
+}
